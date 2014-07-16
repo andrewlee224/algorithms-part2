b5b78951e6945e9ec69df00ac483a4ecd06fd738
andrewlee224 <andrewlee224@gmail.com>
2014-07-16 11:46:43 +0200

    Union-Find data structure

---
+++ week2/unionfind.py
@@ -0,0 +1,75 @@
+"""
+Implementation of a Union-Find data structure
+"""
+
+class Vertex(object):
+    """
+    Vertex with a pointer to a leader vertex.
+    May point to itself.
+    """
+    def __init__(self, key, leader=None):
+        self.componentCount = 0
+        self.key = key
+        # if leader, keeps track of vertices pointing to it
+        self.outbound = []
+
+        if not leader:
+            leader = self
+        self.leader = leader
+        # leader vertex keeps count of all children vertices
+        leader.componentCount += 1
+        leader.outbound.append(self)
+
+class Component(object):
+    """
+    Object representing a group of connected vertices.
+    """
+
+    def __init__(self):
+        self._vdict = {}
+        self.total = 0
+        self.leader = None
+
+    def add(self, vertex):
+        self._vdict[vertex.key] = vertex
+        if not leader:
+            self.leader = vertex
+
+
+class UnionFind(object):
+    
+    def __init__(self, initList=None):
+        self._vdict = {}
+        self.totalVertices = 0
+        if initList:
+            for el in initList:
+                self._vdict[el] = Vertex(el)
+            self.totalVertices = len(initList)    
+
+    def add(self, vertex):
+        self._vdict[vertex.key] = vertex
+
+    def find(self, key):
+        """Return leader of the vertex requested by key"""
+        vertex = self._vdict[key]
+        
+        return vertex.leader
+
+    def union(self, key1, key2):
+        v1 = self._vdict[key1]
+        v2 = self._vdict[key2]
+
+        l1 = v1.leader
+        l2 = v2.leader
+
+        if l1.componentCount >= l2.componentCount:
+            # change leader of vertices in a smaller component
+            for v in l2.outbound:
+                v.leader = l1
+            l1.componentCount += l2.componentCount
+            l1.outbound += l2.outbound
+        else:
+            for v in l1.outbound:
+                v.leader = l2
+            l2.componentCount += l1.componentCount
+            l2.outbound += l1.outbound
