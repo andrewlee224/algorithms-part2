2479898d159b49f17c63ed13fba4d918a4ce909a
andrewlee224 <andrewlee224@gmail.com>
2014-07-16 19:44:50 +0200

    K-clustering algorithm based on Kruskal's MST

---
+++ week2/kclustering.py
@@ -0,0 +1,47 @@
+"""
+K-clustering algorithm using the Union-Find data structure.
+Proceeds similar to Kruskal's MST algorithm, differs in stopping 
+when there are k components of a graph, instead of a connected
+graph.
+"""
+
+import unionfind as uf
+from collections import deque
+
+def getGraph(fPath="clustering1.txt"):
+
+    edgeList = []
+    with open(fPath) as f:
+        lines = f.readlines()
+        numNodes = int(lines[0])
+        for line in lines[1:]:
+            node1, node2, edgeCost = [ int(el) for el in line.split() ]
+            edgeList.append((node1, node2, edgeCost))
+
+    return edgeList, numNodes
+
+def kclustering(k, edgeList, numNodes):
+    
+    # sort edges by their weights ascending, then select in this
+    # order
+    edgeList.sort(key=lambda tup: tup[2])
+    edgeDeque = deque(edgeList)
+    ufStruct = uf.UnionFind(list(range(1, numNodes+1))) # for 1-based
+
+    while (ufStruct.totalComponents > k):
+        # select cheapest edge that does not create cycles
+        candidate = edgeDeque.popleft()
+        ufStruct.union(candidate[0], candidate[1])
+
+    # max spacing is the smallest unused edge
+    maxSpacingEdge = edgeDeque.popleft() 
+
+    return maxSpacingEdge
+
+
+if __name__ == '__main__':
+    edgeList, numNodes = getGraph("clustering1.txt")
+    k = 4
+    maxSpacingEdge = kclustering(k, edgeList, numNodes)
+    
+    print("{}-clustering max spacing: {}".format(k, maxSpacingEdge[2]))
