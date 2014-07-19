74d5fd1adc255c857c3bd36eb9a36d0a47b6f792
andrewlee224 <andrewlee224@gmail.com>
2014-07-19 23:04:46 +0200

    Almost working properly, failing test case #4

--- week2/implclustering.py
+++ week2/implclustering.py
@@ -5,7 +5,8 @@ nodes are defined as the number of differing bits between the
 nodes labels.
 """
 
-from itertools import izip
+import unionfind as uf
+from itertools import izip, combinations
 
 def getGraph(fPath="clustering_big.txt"):
 
@@ -19,7 +20,7 @@ def getGraph(fPath="clustering_big.txt"):
             node = "".join([ el for el in line if el is not " "])[:-1]
             nodeList.append(node)
 
-    return nodeList
+    return nodeList, labelBits
 
 
 def hammingDistance(node1, node2):
@@ -27,4 +28,70 @@ def hammingDistance(node1, node2):
 
     return sum(c1 != c2 for c1, c2 in izip(node1, node2))
 
+def calculateMasks(bitsLabel=24):
+    """
+    Create swap masks in the form tuples with numbers
+    indicating which bits to swap.
+    """
 
+    swapMasks1 = [ (el, ) for el in range(bitsLabel) ]
+    swapMasks2 = [ c for c in combinations(range(bitsLabel), 2) ]
+
+    return swapMasks1 + swapMasks2
+
+def swapByMask(label, mask):
+    """Swap bits in label according to a mask"""
+
+    modLabel = [ c for c in label ]
+
+    for bitNum in mask:
+        if modLabel[bitNum] == '0':
+            modLabel[bitNum] = '1'
+        elif modLabel[bitNum] == '1':
+            modLabel[bitNum] = '0'
+
+    return "".join(modLabel)
+
+def createLabelDict(nodeList):
+    d = {}
+
+    for i, node in enumerate(nodeList):
+        if node not in d:
+            d[node] = [i]
+        else:
+            d[node].append(i)
+
+    return d
+
+def implicitClustering(nodeList, maxDist=3, bitsLabel=24):
+    
+    numNodes = len(nodeList)
+    ufstruct = uf.UnionFind(list(range(len(nodeList))))
+    
+    swapMasks = calculateMasks(bitsLabel)
+    labelDict = createLabelDict(nodeList)
+
+    numClusterings = 0
+    for i, node1 in enumerate(nodeList):
+        print("Node {} of {}".format(i+1, numNodes))
+
+        for mask in swapMasks:
+            modLabel = swapByMask(node1, mask)
+            if modLabel in labelDict:
+                for index in labelDict[modLabel]:
+                    success = ufstruct.union(i, index)
+                    if success:
+                        numClusterings += 1
+
+    k = len(nodeList) - numClusterings
+    return k
+
+
+if __name__ == '__main__':
+    print("Loading graph..")
+    nodeList, labelBits = getGraph()
+    print("Calculating clusters..")
+    maxDist = 3
+    k = implicitClustering(nodeList, maxDist, labelBits)
+    print("Minimum number of clusters to ensure that members " + 
+        "differ at most with 3 bits: {}".format(k))
