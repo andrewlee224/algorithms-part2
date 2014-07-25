f839d4a51af053f971944a84bf38ee24a3b2b5bb
Andrzej Lichaczewski <andrewlee224@gmail.com>
2014-07-25 23:01:57 +0200

    Fixed iterating over all possible weights, test case passed

--- week3/knapsack.py
+++ week3/knapsack.py
@@ -17,20 +17,28 @@ def loadData(fPath="knapsack1.txt"):
 
 
 def optimalSubproblems(items, totalWeight):
+    """
+    Build a 2d array of values of optimal solutions to subproblems
+    depending on considered number of items and knapsack size.
+    """
+
     optArray = [[]]
-    for w in range(totalWeight):
+    # fill row 0 of output array with starting values
+    for w in range(totalWeight+1):
         if items[0][1] <= w:
             optArray[0].append(items[0][0])
         else:
             optArray[0].append(0)
 
+    # row 0 of output array already filled, so continue
+    # filling from row 1 onwards
     for i, item in enumerate(items[1:]):
         i = i + 1
         optArray.append([])
         curVal, curWeight = item
 
-        for w in range(totalWeight):
-            optArray[i].append(0)
+        for w in range(totalWeight+1):
+            optArray[i].append(optArray[i-1][w])
             if curWeight > w:
                 continue
 
@@ -39,7 +47,5 @@ def optimalSubproblems(items, totalWeight):
 
             if v1 >= v2:
                 optArray[i][w] = v1
-            else:
-                optArray[i][w] = v2
 
     return optArray
