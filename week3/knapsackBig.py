5667e0f36adc217b9ad54837722318d3a90e0a7e
andrewlee224 <andrewlee224@gmail.com>
2014-07-27 11:39:32 +0200

    Caching intermediate results, fixed checking if last item has bigger weight than total. Tests passed.

--- week3/knapsackBig.py
+++ week3/knapsackBig.py
@@ -1,8 +1,12 @@
 """
 Implementation of the dynamic programming knapsack algorithm for a very
-large dataset. Standard iterative approach is unfeasible, utilising a recursive approach and caching subproblems solutions instead.
+large dataset. Standard iterative approach is unfeasible, utilising a 
+recursive approach and caching subproblems solutions instead.
 """
 
+import sys
+
+cacheDict = {}
 
 def loadData(fPath="knapsack_big.txt"):
     items = []
@@ -18,27 +22,44 @@ def loadData(fPath="knapsack_big.txt"):
 
 
 def solve(partialItems, totalWeight):
-    print("="*50)
     if totalWeight <= 0:
-        print("Total weight < 0, return 0")
         return 0
 
     lastItem = partialItems[-1]
     if len(partialItems) == 1:
-        print("Last item, return "),
         if lastItem[1] <= totalWeight:
-            print(lastItem[0])
             return lastItem[0]
         else:
-            print("0")
             return 0
 
-    print("Enter first case {}".format(len(partialItems)))
-    val1 = solve(partialItems[:-1], totalWeight)
-    print("Enter second case {}".format(len(partialItems)))
-    val2 = solve(partialItems[:-1], totalWeight - lastItem[1]) + lastItem[0]
+    key1 = (len(partialItems[:-1]), totalWeight)
+    val1 = cacheDict.get(key1)     
+    if val1 is None:        
+        val1 = solve(partialItems[:-1], totalWeight)
+        cacheDict[key1] = val1
+
+    key2 = (len(partialItems[:-1]), totalWeight - lastItem[1])
+    val2pre = cacheDict.get(key2)
+    if val2pre is None:
+        val2pre = solve(partialItems[:-1], totalWeight - lastItem[1])
+        cacheDict[key2] = val2pre
+    if lastItem[1] <= totalWeight:
+        val2 = val2pre + lastItem[0]
+    else:
+        val2 = 0
 
     retVal = val1 if val1 >= val2 else val2
-    print(partialItems, totalWeight)
-    print(totalWeight, val1, val2, retVal)
     return retVal
+
+
+def main():
+    print("Calculating the knapsack optimal solution value..")
+    itemList, totalWeight, numItems = loadData("knapsack_big.txt")
+    sys.setrecursionlimit(numItems+200)
+
+    optVal = solve(itemList, totalWeight)
+        
+    print("Optimal value: {}".format(optVal))
+
+if __name__ == '__main__':
+    main()
