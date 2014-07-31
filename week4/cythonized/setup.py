ee4150c18cd009ec73b1bedc5833061ba35dd643
Andrzej Lichaczewski <andrewlee224@gmail.com>
2014-08-01 00:27:58 +0200

    Modified to detect numpy

--- week4/cythonized/setup.py
+++ week4/cythonized/setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension("floydwarshall", sources=["floydwarshall.pyx"])
+]
 
 setup(
-        ext_modules = cythonize("floydwarshall.pyx")
+        name="floydwarshall",
+        include_dirs=[np.get_include()],
+        ext_modules = cythonize(extensions)
 )
