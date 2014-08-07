1cfc9cead9724e23cd73513d22de095cc0b3e7ed
Andrzej Lichaczewski <andrewlee224@gmail.com>
2014-08-07 17:40:32 +0200

    Algorithm gives accurate results

--- week5/tsp.py
+++ week5/tsp.py
@@ -5,10 +5,6 @@ Dynamic programming algorithm for solving the Traveling Salesman Problem
 import numpy as np
 
 
-def distance(x1, y1, x2, y2):
-    return ((x1 - x2)**2 + (y1 - y2)**2)**0.5
-
-
 def getPoints(fPath="tsp.txt"):
     with open(fPath, 'r') as fObj:
         lines = fObj.readlines()
@@ -51,7 +47,7 @@ def computeSets(maxCardinality):
 
 
 def onesIndices(num):
-    """Returns 1-based indices of set bits"""
+    """Returns 0-based indices of set bits"""
 
     onesIndices = []
     bl = num.bit_length()
@@ -66,36 +62,29 @@ def tsp(points, sets, distances):
     subproblemArr = np.ones((2**(len(points)), len(points)),
                                 dtype='float')
     subproblemArr = subproblemArr * float('inf')
-   
+
     # initialize base cases
     for i in range(2**(len(points))):
         subproblemArr[i, 0] = float('inf')
     subproblemArr[1, 0] = 0
 
-    #for routeSet in sets[1]:
-    #    j = [ i for i, j in enumerate(bin(routeSet)) if j == '1' ][0]
-    #    subproblemArr[routeSet, j] = distances[0, j]
-
     for m in range(2, len(points)+1):
+        print("Computing subproblems of size {}".format(m))
         for routeSet in sets[m]:
             # make sure that first vertex is in the route
             if routeSet & 0b1 != 0b1:
                 continue
 
             # binary numbers are represented as strings, e.g. '0b10101'
-            jCandidates = onesIndices(routeSet) # [ i-2 for i, j in enumerate(bin(routeSet)) if j == '1' ]
-            #print("jCandidates: {}".format(jCandidates))
+            jCandidates = onesIndices(routeSet)
             for j in jCandidates:
                 if jCandidates == 0:
                     continue
 
-                xorMask = 2**j # bin('0b' + '0' * j + '1')
+                xorMask = 2**j
                 previousSet = routeSet ^ xorMask
-                #print(bin(routeSet))
-                #print(bin(previousSet))
-                #print('='*50)
                 bestVal = float('inf')
-                
+
                 for k in jCandidates:
                     if k == j:
                         continue
@@ -110,3 +99,21 @@ def tsp(points, sets, distances):
         shortestRoute = val if val < shortestRoute else shortestRoute
 
     return subproblemArr, shortestRoute
+
+
+def main():
+    print("Loading data..")
+    points, numPoints = getPoints()
+    print("Computing possible route sets")
+    sets = computeSets(len(points))
+    print("Computing distances")
+    distances = computeDistances(points)
+
+    print("Computing optimal subproblems")
+    subproblems, minRouteVal = tsp(points, sets, distances)
+
+    print("Minimum route value: {}".format(minRouteVal))
+
+
+if __name__ == '__main__':
+    main()
